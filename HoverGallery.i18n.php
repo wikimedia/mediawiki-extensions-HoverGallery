071dde7bfa1151430a446fd13b2dbe010462c68e
raymond <raimond.spekking@gmail.com>
2013-07-15 17:55:48 +0200

    Consistency tweaks in preparation for adding extension to translatewiki.net
    * Remove period from end of description
    * Add headers
    
    Change-Id: Ifd7246ca90e2e6263d5c946f59cdd473eef91a3e

--- HoverGallery.i18n.php
+++ HoverGallery.i18n.php
@@ -8,10 +8,23 @@
 
 $messages = array();
 
+/** English
+ * @author Luis Felipe Schenone
+ */
 $messages['en'] = array(
-	'hovergallery-desc' => 'Adds a <code>&lt;hovergallery&gt;</code> tag that creates galleries like the standard ones, except that when the mouse hovers over the thumbnails, the full-size images are displayed.',
+	'hovergallery-desc' => 'Adds a <code>&lt;hovergallery&gt;</code> tag that creates galleries like the standard ones, except that when the mouse hovers over the thumbnails, the full-size images are displayed',
 );
 
+/** Message documentation (Message documentation)
+ * @author Raimond Spekking
+ */
+$messages['qqq'] = array(
+	'hovergallery-desc' => '{{desc|name=HoverGallery|url=https://www.mediawiki.org/wiki/Extension:HoverGallery}}',
+);
+
+/** Spanish
+ * @author Luis Felipe Schenone
+ */
 $messages['es'] = array(
-	'hovergallery-desc' => 'Agrega una etiqueta <code>&lt;hovergallery&gt;</code> que crea galerías como las estándar, excepto que cuando el mouse pasa sobre los thumbnails, se muestra la imagen completa.',
+	'hovergallery-desc' => 'Agrega una etiqueta <code>&lt;hovergallery&gt;</code> que crea galerías como las estándar, excepto que cuando el mouse pasa sobre los thumbnails, se muestra la imagen completa',
 );
