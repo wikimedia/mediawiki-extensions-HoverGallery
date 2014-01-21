4aa646aa33aaae9b038b2f5d74f53da8b5475cd0
Luis Felipe Schenone <schenonef@gmail.com>
2014-01-21 14:17:57 -0300

    Updated the workings of the maxhover parameters
    Change-Id: I8196963f33dd5d47d803a1d451f8d58475082e35

--- HoverGallery.body.php
+++ HoverGallery.body.php
@@ -14,17 +14,17 @@ class HoverGallery {
 
 	public static function renderGallery( $input, array $ARGS, Parser $parser, PPFrame $frame ) {
 
-		$maxhoverwidth = '100%';
-		$maxhoverheight = '100%';
-		if ( array_key_exists( 'hoversize', $ARGS ) ) {
-			$maxhoverwidth = $ARGS['hoversize'] . 'px';
-			$maxhoverheight = $ARGS['hoversize'] . 'px';
+		$maxhoverwidth = '';
+		$maxhoverheight = '';
+		if ( array_key_exists( 'maxhoversize', $ARGS ) ) {
+			$maxhoverwidth = $ARGS['maxhoversize'];
+			$maxhoverheight = $ARGS['maxhoversize'];
 		}
 		if ( array_key_exists( 'maxhoverwidth', $ARGS ) ) {
-			$maxhoverwidth = $ARGS['maxhoverwidth'] . 'px';
+			$maxhoverwidth = $ARGS['maxhoverwidth'];
 		}
 		if ( array_key_exists( 'maxhoverheight', $ARGS ) ) {
-			$maxhoverheight = $ARGS['maxhoverheight'] . 'px';
+			$maxhoverheight = $ARGS['maxhoverheight'];
 		}			
 
 		$normalGallery = $parser->recursiveTagParse( '<gallery>' . $input . '</gallery>' );
@@ -33,7 +33,11 @@ class HoverGallery {
 		$FILENAMES = explode( "\n", $input );
 		$FILENAMES = array_filter( $FILENAMES );
 		foreach ( $FILENAMES as $filename ) {
-			$hiddenGallery .= $parser->recursiveTagParse( '[[' . $filename . ']]' );
+			if ( $maxhoverwidth or $maxhoverheight ) {
+				$hiddenGallery .= $parser->recursiveTagParse( '[[' . $filename . '|' . $maxhoverwidth . 'x' . $maxhoverheight . 'px]]' );
+			} else {
+				$hiddenGallery .= $parser->recursiveTagParse( '[[' . $filename . ']]' );
+			}
 		}
 		$hiddenGallery .= '</div>';
 
