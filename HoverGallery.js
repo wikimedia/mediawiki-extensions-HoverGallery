a4bef2afe668451d679423d7058ceffc8aa4f19c
Umherirrender <umherirrender_de.wp@web.de>
2017-07-16 00:47:27 +0200

    Fix jshint errors
    21:08:58 Running "jshint:all" (jshint) task
    21:08:58 
    21:08:58    HoverGallery.js
    21:08:58      27 |            url = $( '<span>' ).html( url ).text(); //
    Decode the HTML entities in the URL
    21:08:58                      ^ 'url' is already defined.
    21:08:58      13 |        var loadingUrl = mw.config.get(
    'wgExtensionAssetsPath' ) + '/Hovergallery/images/loading.gif',
    21:08:58                                   ^ 'mw' is not defined.
    21:08:58 
    21:08:58 >> 2 errors in 2 files
    
    Change-Id: I2f4b50321457c4e92b3208cad6f2083a918a7928

--- HoverGallery.js
+++ HoverGallery.js
@@ -1,45 +1,47 @@
-var HoverGallery = {
+( function ( $, mw ) {
+	var HoverGallery = {
 
-	init: function () {
-		HoverGallery.bind();
-	},
+		init: function () {
+			HoverGallery.bind();
+		},
 
-	bind: function () {
-		$( '.gallery img' ).hover( HoverGallery.onMouseEnter, HoverGallery.onMouseLeave );
-	},
+		bind: function () {
+			$( '.gallery img' ).hover( HoverGallery.onMouseEnter, HoverGallery.onMouseLeave );
+		},
 
-	onMouseEnter: function () {
-		// First show the loading icon
-		var loadingUrl = mw.config.get( 'wgExtensionAssetsPath' ) + '/Hovergallery/images/loading.gif',
-			loadingImg = $( '<img>' ).addClass( 'hoverimage' ).attr( 'src', loadingUrl );
-		$( 'body' ).append( loadingImg );
+		onMouseEnter: function () {
+			// First show the loading icon
+			var loadingUrl = mw.config.get( 'wgExtensionAssetsPath' ) + '/Hovergallery/images/loading.gif',
+				loadingImg = $( '<img>' ).addClass( 'hoverimage' ).attr( 'src', loadingUrl );
+			$( 'body' ).append( loadingImg );
 
-		// Get the data from the gallery
-		var gallery = $( this ).closest( '.gallery' ),
-			fileUrls = gallery.data( 'hovergallery-fileurls' ),
-			maxHoverWidth = gallery.data( 'hovergallery-maxhoverwidth' ),
-			maxHoverHeight = gallery.data( 'hovergallery-maxhoverheight' );
+			// Get the data from the gallery
+			var gallery = $( this ).closest( '.gallery' ),
+				fileUrls = gallery.data( 'hovergallery-fileurls' ),
+				maxHoverWidth = gallery.data( 'hovergallery-maxhoverwidth' ),
+				maxHoverHeight = gallery.data( 'hovergallery-maxhoverheight' );
 
-		// Determine which of the thumbs is it
-		var thumbs = $( 'img', gallery ),
-			thumbIndex = $.inArray( this, thumbs ),
-			url = fileUrls[ thumbIndex ],
-			url = $( '<span>' ).html( url ).text(); // Decode the HTML entities in the URL
+			// Determine which of the thumbs is it
+			var thumbs = $( 'img', gallery ),
+				thumbIndex = $.inArray( this, thumbs ),
+				fileUrl = fileUrls[ thumbIndex ],
+				url = $( '<span>' ).html( fileUrl ).text(); // Decode the HTML entities in the URL
 
-		// Replace the loading icon with the image
-		var image = new Image();
-		image.src = url;
-		image.onload = function () {
-			loadingImg.css({
-				'max-width': maxHoverWidth + 'px',
-				'max-height': maxHoverHeight + 'px'
-			}).attr( 'src', url );
-		};
-	},
+			// Replace the loading icon with the image
+			var image = new Image();
+			image.src = url;
+			image.onload = function () {
+				loadingImg.css({
+					'max-width': maxHoverWidth + 'px',
+					'max-height': maxHoverHeight + 'px'
+				}).attr( 'src', url );
+			};
+		},
 
-	onMouseLeave: function () {
-		$( 'body .hoverimage' ).remove();
-	}
-};
+		onMouseLeave: function () {
+			$( 'body .hoverimage' ).remove();
+		}
+	};
 
-$( HoverGallery.init );
+	$( HoverGallery.init );
+}( jQuery, mediaWiki ) );
