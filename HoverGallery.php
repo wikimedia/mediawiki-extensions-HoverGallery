caa630d6db985ea09c1600d61a78c86b674c7da8
zoranzoki21 <zorandori4444@gmail.com>
2020-04-13 22:46:14 +0200

    Add phpcs and make it pass
    Change-Id: I3e05d7d86e7f93c10eca2996c42b4bd22b0adf40

--- HoverGallery.php
+++ HoverGallery.php
@@ -2,40 +2,55 @@
 
 class HoverGallery {
 
-	static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+	/**
+	 * @param OutputPage &$out
+	 * @param Skin &$skin
+	 */
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 		$out->addModules( 'ext.HoverGallery' );
-		return true;
-	}
-
-	static function onParserFirstCallInit( Parser &$parser ) {
-		$parser->setHook( 'hovergallery', 'HoverGallery::render' );
-		return true;
 	}
 
 	/**
+	 * @param Parser &$parser
+	 */
+	public static function onParserFirstCallInit( Parser &$parser ) {
+		$parser->setHook( 'hovergallery', 'HoverGallery::render' );
+	}
+
+	/**
+	 * @param string $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return string
 	 * @suppress SecurityCheck-DoubleEscaped
 	 */
-	static function render( $input, array $ARGS, Parser $parser, PPFrame $frame ) {
-
+	public static function render( $input, array $args, Parser $parser, PPFrame $frame ) {
 		$maxhoverwidth = 640;
 		$maxhoverheight = 640;
-		if ( array_key_exists( 'maxhoversize', $ARGS ) ) {
-			$maxhoverwidth = $ARGS['maxhoversize'];
-			$maxhoverheight = $ARGS['maxhoversize'];
+		if ( array_key_exists( 'maxhoversize', $args ) ) {
+			$maxhoverwidth = $args['maxhoversize'];
+			$maxhoverheight = $args['maxhoversize'];
 		}
-		if ( array_key_exists( 'maxhoverwidth', $ARGS ) ) {
-			$maxhoverwidth = $ARGS['maxhoverwidth'];
+		if ( array_key_exists( 'maxhoverwidth', $args ) ) {
+			$maxhoverwidth = $args['maxhoverwidth'];
 		}
-		if ( array_key_exists( 'maxhoverheight', $ARGS ) ) {
-			$maxhoverheight = $ARGS['maxhoverheight'];
+		if ( array_key_exists( 'maxhoverheight', $args ) ) {
+			$maxhoverheight = $args['maxhoverheight'];
 		}
 
-		$FILEURLS = array();
+		$FILEURLS = [];
 		$FILENAMES = array_filter( explode( PHP_EOL, trim( $input ) ) );
 		foreach ( $FILENAMES as $filename ) {
-			$filename = strtok( $filename, '|' ); // Remove the options
-			$filename = substr( $filename, ( $position = strpos( $filename, ':' ) ) === false ? 0 : $position + 1 ); // Remove the File prefix
-			$filepath = $parser->recursiveTagParse( '{{filepath:' . $filename . '|nowiki}}' ); // Get the filepath
+			// Remove the options
+			$filename = strtok( $filename, '|' );
+			// Remove the File prefix
+			$filename = substr(
+				$filename,
+				( $position = strpos( $filename, ':' ) ) === false ? 0 : $position + 1
+			);
+			// Get the filepath
+			$filepath = $parser->recursiveTagParse( '{{filepath:' . $filename . '|nowiki}}' );
 			$FILEURLS[] = $filepath;
 		}
 
